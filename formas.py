17e80ba6e0a9fc8600e7e13e96743b0a89e54cc3
Isaac Alfredo <isaacfreitas23@gmail.com>
2022-10-11 15:00:01 -0300

    exemplos

--- formas.py
+++ formas.py
@@ -14,8 +14,6 @@ def quadrado(x,y,transfMatriz):
     clinha = list(np.matmul(transfMatriz,c))
     dlinha = list(np.matmul(transfMatriz,d))
 
-    print(alinha,blinha,clinha,dlinha)
-
     glBegin(GL_QUADS) # Begin the sketch
     glVertex2f(alinha[0], alinha[1]) # Coordinates for the bottom left point
     glVertex2f(blinha[0], blinha[1]) # Coordinates for the bottom right point
@@ -23,19 +21,29 @@ def quadrado(x,y,transfMatriz):
     glVertex2f(dlinha[0], dlinha[1]) # Coordinates for the top left point
     glEnd()
 
-def triangulo(x,y):
+def triangulo(x,y,transfMatriz):
+    a = [x,y,1] #ponto inferior esquerdo
+    b = [x+100,y,1] #ponto inferior direito
+    c = [x,y+100,1] #ponto superior
+
+    alinha = list(np.matmul(transfMatriz,a))
+    blinha = list(np.matmul(transfMatriz,b))
+    clinha = list(np.matmul(transfMatriz,c))
+
     glBegin(GL_TRIANGLES) # Begin the sketch
-    glVertex2f(x, y) # Coordinates for the bottom left point
-    glVertex2f(x+100, y) # Coordinates for the bottom right point
-    glVertex2f(x, y+100) # Coordinates for the top right point
+    glVertex2f(alinha[0], alinha[1]) # Coordinates for the bottom left point
+    glVertex2f(blinha[0], blinha[1]) # Coordinates for the bottom right point
+    glVertex2f(clinha[0], clinha[1]) # Coordinates for the top right point
     glEnd()
 
-def circulo(x,y):
+def circulo(x,y,transfMatriz):
     circlePoints = 100
     pi = 3.14
     raio=100
     glBegin(GL_LINE_LOOP)
     for i in range(circlePoints):
         theta=(2*pi*i)/circlePoints
-        glVertex2f(x+(raio*np.cos(theta)),y+(raio*np.sin(theta)))
+        p = [x+(raio*np.cos(theta)),y+(raio*np.sin(theta)),1]
+        plinha = list(np.matmul(transfMatriz,p))
+        glVertex2f(plinha[0],plinha[1])
     glEnd()
--- showscreen.py
+++ showscreen.py
@@ -2,12 +2,11 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
 import formas
-from menu2d import matResult
+from menu2d import matResult,menu2d
 import operacoes as op
 import numpy as np
 
 def transfExemplo():
-    pontoInicial = [[1,1,1]]
     listaMatrizes = []
     listaMatrizes.append(op.escala(2,2))
     listaMatrizes.append(op.translacao(100,100))
@@ -27,30 +26,29 @@ def iterate():
     #glLoadIdentity()
 
 def showScreenQuadrado():
-    pontoFinal = transfExemplo()
-
     #glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT) # Remove everything from screen (i.e. displays all white)
     #glLoadIdentity() # Reset all graphic/shape's position
     iterate()
     glColor3f(1.0, 0.0, 3.0) # Set the color to pink
-    formas.quadrado(1,1,transfExemplo())
-    glColor3f(50.0, 50.0, 50.0) # Set the color to pink
-    formas.quadrado(1,1,np.identity(3))
+    formas.quadrado(0,0,transfExemplo())
+    glColor3f(50.0, 50.0, 50.0)
+    formas.quadrado(0,0,np.identity(3))
     glutSwapBuffers()
 
 def showScreenTriangulo():
-    #glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT) # Remove everything from screen (i.e. displays all white)
-    #glLoadIdentity() # Reset all graphic/shape's position
     iterate()
     glColor3f(1.0, 0.0, 3.0) # Set the color to pink
-    formas.triangulo(0,0)
+    formas.triangulo(0,0,transfExemplo())
+    glColor3f(50.0, 50.0, 50.0)
+    formas.triangulo(0,0,np.identity(3))
     glutSwapBuffers()
 def showScreenCirculo():
-    #glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT) # Remove everything from screen (i.e. displays all white)
-    #glLoadIdentity() # Reset all graphic/shape's position
     iterate()
     glColor3f(1.0, 0.0, 3.0) # Set the color to pink
-    formas.circulo(250,250)
+    formas.circulo(0,0,transfExemplo())
+    glColor3f(50.0, 50.0, 50.0)
+    formas.circulo(0,0,np.identity(3))
+
     glutSwapBuffers()
 
 #print(np.matmul(transfExemplo(),[1,1,1]))
